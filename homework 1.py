80006a6a45992780760592d81e029d57d1709da3
Honor <annalanovskaa6@gmail.com>
2025-10-04 11:43:08 +0200

    hw

---
+++ homework 1.py
@@ -0,0 +1,17 @@
+age = 16
+name = 'Anna'
+#print(id(name))
+#print(type(age))    #task 1
+
+
+
+my_list = [1, 3, 'text', (5, 7, 9), 2.18]
+#print(my_list)      #task 2
+#print(my_list.__sizeof__() / 8)    #task 3
+
+
+a = input('Number 1: ')
+b = input('Number 2: ')
+a = int(a)
+b = int(b)
+print(a + b)       #task 4
