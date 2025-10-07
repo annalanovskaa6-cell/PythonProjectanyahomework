e7deaa6069bc3d4786d8ebc95f013ab66945ed42
Honor <annalanovskaa6@gmail.com>
2025-10-07 16:23:29 +0200

    hw

---
+++ homework 2.py
@@ -0,0 +1,19 @@
+a = input("Word 1: ")
+b = input("Word 2: ")
+c = input("Word 3: ")
+#print(f'{a} - {b} - {c}.')      #task 1
+
+
+num_1 = int(input('Number 1: '))
+num_2 = float(input('Number 2: '))
+num_3 = str(input('Number 3: '))
+#print(num_1, num_2, num_3, sep=',')     #task 2
+
+
+line = "a.b.g.d.l.m.r.t.k.n.m.t.p.w.g.f.k.b.i.e.k.t.h.k.b.a.y.w.j"
+letters_list = line.split('.')
+#print(letters_list)       #task 3
+
+
+text = "Hello, \n\tFututre Step"
+#print(text)        #task 4
