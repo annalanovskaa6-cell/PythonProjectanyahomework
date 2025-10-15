cccb65881accedceeefdb2d01ec4a8c01b7cbb31
Honor <annalanovskaa6@gmail.com>
2025-10-15 15:11:06 +0200

    hw

---
+++ homework 4.py
@@ -0,0 +1,50 @@
+num = int(input())
+result = True if num % 5 == 0 else False
+# print(result)        #task 1
+
+
+
+num = int(input())
+result = True if 10 <= num <= 20 else False
+# print(result)        #task 2
+
+
+
+#x = int(input())
+#y = int(input())
+#match(x, y):
+#     case(0, 0):
+#         print("Точка находится в начале координат")
+#     case(0, _):
+#         print("Точка лежит на оси Y")
+#     case(_, 0):
+#         print("Точка лежит на оси X")
+#     case(x, y) if x > 0 and y >0:
+#         print("Точка находится в первой четверти")
+#     case(x, y) if x < 0 and y > 0:
+#         print("Точка находится во второй четверти")
+#     case(x, y) if x < 0 and y < 0:
+#         print("Точка находится в третьей четверти")
+#     case(x, y) if x > 0 and y < 0:
+#         print("Точка находится в четвёртой четверти")       #task 3
+
+
+
+# operation = input("Введите операцию(+, -, *, /): ")
+# num_1 = int(input("Введите первое число: "))
+# num_2 = int(input("Введите второе число: "))
+# match operation:
+#      case "+":
+#          print(num_1 + num_2)
+#      case "-":
+#          print(num_1 - num_2)
+#      case "*":
+#          print(num_1 * num_2)
+#      case "/" if num_2 != 0:
+#          print(num_1 / num_2)
+
+
+
+
+
+
