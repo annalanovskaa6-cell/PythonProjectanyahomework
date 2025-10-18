1f09f06a001376aaedea2da18acd2ca2d1a632da
Honor <annalanovskaa6@gmail.com>
2025-10-18 17:15:34 +0200

    hw

---
+++ homework 5.py
@@ -0,0 +1,69 @@
+# for i in range(1, 20):
+#     if i % 2 != 0:
+#         print(i)      #task 1
+
+
+
+# num = input("Введите целое число: ")
+# sum = 0
+# for digit in num:
+#     sum += int(digit)
+# print("Сумма цифр: ", sum)       #task 2
+
+
+
+# nums = input("Введите несколько чисел: ").split()
+# found = False
+# for num in nums:
+#     if int(num) > 100:
+#         print(num)
+#         found = True
+#         break
+# if not found:
+#     print("Таких чисел нет")      #task 3
+
+
+
+# num = int(input())
+# found = False
+# for i in range(2, num):
+#     if num % i == 0:
+#         print(i)
+#         found = True
+#         break
+# if not found:
+#     print("Число простое")       #task 4
+
+
+
+# for i in range(1, 5):
+#     for j in range(i):
+#         print('*', end='')
+#     print()        #task 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
