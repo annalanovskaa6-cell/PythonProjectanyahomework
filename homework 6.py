ebec01deaddfef05bff1ef8391b40aca89abeace
Honor <annalanovskaa6@gmail.com>
2025-10-22 14:57:55 +0200

    hw

---
+++ homework 6.py
@@ -0,0 +1,26 @@
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# for v_1 in alphabet:
+#     for v_2 in alphabet:
+#         for v_3 in alphabet:
+#             password = v_1 + v_2 + v_3
+#             print(password)       #task 1
+
+
+
+# while True:
+#     s = input('Введите yes чтобы нарисовать, что-то другое чтобы пропустить: ')
+#     if s == 'yes':
+#         for i in range(1, 5):
+#             print('*' * i)
+#         break
+#     else:
+#         print('не нарисую')        #task 2
+
+
+
+# s = input("Введите строку: ")
+# s = s.replace(" ", "").lower()
+# if s == s[::-1]:
+#     print("Да, это палиндром")
+# else:
+#     print("Нет, это не палиндром")       #task 4
