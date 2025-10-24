803b41ad8d16676dbcdbb1109eeb59093b815ce4
Honor <annalanovskaa6@gmail.com>
2025-10-24 17:50:30 +0200

    hw

---
+++ homework 7.py
@@ -0,0 +1,71 @@
+# for i in range(100, 0, -10):
+#     print(i)       #task 1
+
+
+
+# while True:
+#     num = int(input("Введите число: "))
+#     print("Введено число: ", num)
+#     if num % 5 == 0:
+#         print("Число кратно 5, цикл завершён!")
+#         break       #task 2
+
+
+
+# num = int(input("Введите число: "))
+# for i in range(1, num + 1):
+#     for j in range(1, num + 1):
+#         k = i * j
+#         print(f"{i} * {j} = {k}")         #task 3
+
+
+
+# a = int(input("Введите первое число: "))
+# b = int(input("Введите второе число: "))
+# if a > b:
+#     print("Ошибка")
+# else:
+#     summ = 0
+#     for i in range(a, b+1):
+#         for j in range(1):
+#             summ += i ** 2
+#     print("Сумма квадратов чисел: ", summ)        #task 4
+
+
+
+# h = int(input("Введите высоту пирамиды: "))
+# for i in range(1, h + 1):
+#     for j in range(1, i + 1):
+#         print(j, end=" ")
+#     for k in range(i - 1, 0, -1):
+#         print(k, end=" ")
+#     print()            #task 5
+
+
+
+# s = float(input("Введите начальную сумму: "))
+# r = float(input("Введите годовую ставку: "))
+# y = int(input("Введите длительность вклада: "))
+# year = 1
+# while year <= y:
+#     s = s * (1 + r / 100)
+#     print(f"Сумма на конец {year}-го года: {s:.3f}")
+#     year += 1             #task 6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
