2d861bde65e3e3816d7358049c3c205a4868ae7d
Honor <annalanovskaa6@gmail.com>
2025-10-29 15:14:49 +0200

    hw

---
+++ homework 8.py
@@ -0,0 +1,37 @@
+# s = input("Введите вашу строку: ")
+# print(s[::-1])        #task 1
+
+
+
+# s = input("Введите вашу строку: ")
+# print(s[::2])           #task 2
+
+
+
+# s = input("Введите вашу строку: ")
+# if len(s)  >= 10:
+#     print(s[:5] + s[-5:])
+# else:
+#     print("Строка слишком короткая, попробуйте снова!")          #task 3
+
+
+
+# s = input("Введите вашу строку: ")
+# print(len(s))              #task 4
+
+
+
+# s = input("Введите вашу строку: ")
+# print(s.index("o"))         #task 5 ver.1
+
+# s = input("Введите вашу строку: ")
+# ind = s.find("o")
+# if  ind != -1:
+#     print("Индекс буквы: ", ind)
+# else:
+#     print("Ошибка!")          #task 5 ver.2
+
+
+
+# s = input("Введите вашу строку: ")
+# print(s.rfind("a"))           #task 6
