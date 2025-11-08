02d4d166c2a86057043616cd5644f22bf55402d2
Honor <annalanovskaa6@gmail.com>
2025-11-08 17:22:31 +0200

    hw

---
+++ homework 11.py
@@ -0,0 +1,28 @@
+# n = int(input("Введите количество элементов: "))
+# current = 1
+# for _ in range(n):
+#     print(current)
+#     current /= -2         #task 1
+
+
+
+# s = input("Введите строку: ")
+# chr = input("Введите символ для поиска: ")
+# if len(chr) != 1:
+#     print("Ошибка: нужно ввести ровно один символ")
+# else:
+#     count = 0
+#     for symbol in s:
+#         if symbol == chr:
+#             count += 1
+#     print(f"Символ '{chr}' встречается {count} раз(а)")       #task 2
+
+
+
+# s = input("Введите строку: ")
+# length = len(s)
+# for i in range(length):
+#     print(f"Индекс: {i}, Символ: '{s[i]}'")         #task 3
+
+
+
