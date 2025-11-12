d2cddbcf152bdb0d2c20ca30ce9c1d21b850741e
Honor <annalanovskaa6@gmail.com>
2025-11-12 17:14:37 +0200

    hw

---
+++ homework 12.py
@@ -0,0 +1,17 @@
+# list = [14, 2, 72, 93, 45, 28, 13, 99, 27, 36, 19, 59, 15, 74, 71]
+# min_value = min(list)
+# max_value = max(list)
+# min_index = list.index(min_value)
+# max_index = list.index(max_value)
+# start_index = min(min_index, max_index)
+# end_index = max(min_index, max_index)
+# result_list = list[start_index + 1 : end_index]
+# print(result_list)             #task 1
+
+
+
+# number = input("Введите число: ")
+# digits = []
+# for digit in number:
+#     digits.append(int(digit))
+# print(digits)           #task 2
