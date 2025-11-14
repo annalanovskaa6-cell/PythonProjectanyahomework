0851c26401245861c3b187023b1f2c7422028b47
Honor <annalanovskaa6@gmail.com>
2025-11-14 13:50:11 +0200

    hw

---
+++ homework 13.py
@@ -0,0 +1,36 @@
+# set1 = {1, 2, 3}
+# set2 = {4, 5, 6}
+# set3 = set1.union(set2)
+# print(set3)          #task 1
+
+
+
+# fruits = ["яблоко", "банан", "яблоко", "апельсин", "банан", "киви"]
+# unique = set(fruits)
+# print(unique, len(unique))         #task 2
+
+
+
+# A = {1, 2, 3, 4}
+# B = {3, 4, 5, 6}
+# C = A.intersection(B)
+# print(C)          #task 3
+
+
+
+# my_tuple = ('line1', 'line2', 'line3')
+# a, b, c = my_tuple
+# print(b)          #task 4
+
+
+
+# my_tuple = (1, 2, 3, 4, 5)
+# one, *rest = my_tuple
+# print(one, rest)          #task 5
+
+
+
+# t = (1, 2)
+# a = (3, 4)
+# res = t + a
+# print(res)          #task 6
