3eb588d9bff6c2f8f615ec2b61250df95892c7e1
Honor <annalanovskaa6@gmail.com>
2025-11-18 15:36:08 +0200

    hw

---
+++ homework 14.py
@@ -0,0 +1,28 @@
+# import random
+# numbers = [random.randint(1,7) for i in range(7)]
+# unique = set(numbers)
+# duplicate = len(numbers) - len(unique)
+# print(numbers)
+# print(unique)
+# print(duplicate)          #task 1
+
+
+
+# my_set = set()
+# for i in range(5):
+#     num = int(input("Enter a number: "))
+#     my_set.add(num)
+# print(my_set)
+# print(len(my_set))
+# print(min(my_set))
+# print(max(my_set))          #task 2
+
+
+
+# fantasy_readers = {"Игорь", "Катя", "Лев", "Марина"}
+# detective_readers = {"Никита", "Катя", "Лев", "Ольга"}
+# sci_fi_readers = {"Никита", "Павел", "Лев", "Марина"}
+# set_1 = fantasy_readers.intersection(detective_readers, sci_fi_readers)
+# set_2 = fantasy_readers - detective_readers - sci_fi_readers
+# print(set_1)
+# print(set_2)          #task 3
