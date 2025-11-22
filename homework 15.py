fd5e0d249cd24419a92f2cf69823f17bf442bbd4
Honor <annalanovskaa6@gmail.com>
2025-11-22 13:25:30 +0200

    hw

---
+++ homework 15.py
@@ -0,0 +1,26 @@
+# s = input("Введите строку: ")
+# s = s.lower()
+# char_dict = {}
+# for char in s:
+#     if char in char_dict:
+#         char_dict[char] += 1
+#     else:
+#         char_dict[char] = 1
+# print("Словарь с количеством повторений символов: ")
+# print(char_dict)         #task 1
+
+
+
+# numbers = input("Введите числа: ")
+# list = list(map(int, numbers.split()))
+# tuple = tuple(list)
+# print("Кортеж: ", tuple)
+# dict = {}
+# for num in tuple:
+#     if num in dict:
+#         dict[num] += 1
+#     else:
+#         dict[num] = 1
+# print("Словарь с количеством вхождений: ")
+# for key, value in dict.items():
+#     print(f"{key}: {value}")          #task 2
