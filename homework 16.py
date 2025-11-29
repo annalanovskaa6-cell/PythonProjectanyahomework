762a56cc9d8c84e2d2580c4f7d2837f33403e407
Honor <annalanovskaa6@gmail.com>
2025-11-29 17:29:42 +0200

    hw

--- homework 16.py
+++ homework 16.py
@@ -5,12 +5,12 @@
 #             result = result * i
 #             return result
 #         else:
-#             return 'n has to be positive'
+#             return 'n has to be positive'   #task 1
 
 
 # def number_to_digits(number):
 #     digits = list(map(int, str(number)))
-#     return digits
+#     return digits   #task 2
 
 
 
@@ -18,4 +18,4 @@
 # def is_prime(n):
 #     if n <= 1 or (n % 2 == 0 and n > 2):
 #         return False
-#     return all(n % i for i in range(3, int(sqrt(n)) + 1, 2))
+#     return all(n % i for i in range(3, int(sqrt(n)) + 1, 2))    #task 3
