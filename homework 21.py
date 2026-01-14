8a1f6ed2c18e21e2e4bd0f8b08b9b97e2e44ea03
Honor <annalanovskaa6@gmail.com>
2026-01-14 15:24:24 +0200

    hw

---
+++ homework 21.py
@@ -0,0 +1,30 @@
+# lists = [[2, 4, 6], [8, 10, 12], [14, 16, 18]]
+# for sub in lists:
+#     for number in sub:
+#         if number % 2 != 0:
+#             print(f"В подсписке {sub} есть нечетное число")
+#             break
+#         else:
+#             print(f"Подсписок {sub} содержит только четные числа")             #task 1
+
+
+
+# keys = ['name', 'age', 'city', 'profession']
+# values = ['Иван', 28, 'Москва', 'Программист']
+# dictionar = dict(zip(keys, values))
+# for i, key in enumerate(keys):
+#     print(f"{i+1}. {key}: {dictionar[key]}")              #task 2
+
+
+
+# n_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# f_list = [num for sublist in n_list for num in sublist]
+# print("Исходный список:", n_list)
+# print("Одномерный список:", f_list)                 #task 4
+
+
+
+# names = ['Alice', 'Bob', 'Charlie']
+# ages = [25, 30, 35]
+# res_dict = dict(zip(names, ages))
+# print(res_dict)                            #task 5
